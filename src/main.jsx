01d524f9adab6521fcfacab5e435ca781c381a90
muntasir <sajeebmuntasir0@gmail.com>
2023-01-28 11:52:44 +0600

    redux removed

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,14 @@ import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthProvider from './Contexts/AuthProvider';
-import { Provider } from 'react-redux';
-import { store } from './app/store/store';
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <Provider store={store}>
+
         <App />
-      </Provider>
+
     </AuthProvider>
   </React.StrictMode>,
 )
